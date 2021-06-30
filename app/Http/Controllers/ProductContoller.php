0e8863ee9bad7f877e4fcb5973274be6a42a3ff6
Süleyman Başbuğ <escdewil@gmail.com>
2021-06-30 17:42:17 +0300

    photo

---
+++ app/Http/Controllers/ProductContoller.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Brand;
+use App\Models\Image;
+
+class ProductContoller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.list',compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        $brands = Brand::all();
+        return view('products.create',compact('categories','brands'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $request;
+        return $request->file('file');
+        $request->validate([
+            'file' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        $image = new Image;
+        if ($request->file('file')) {
+            $imagePath = $request->file('file');
+            $imageName = $imagePath->getClientOriginalName();
+
+            $path = $request->file('file')->storeAs('uploads', $imageName, 'public');
+        }
+        $image->name = $imageName;
+        $image->path = '/storage/'.$path;
+        $image->product_id = 1;
+        $image->save();
+
+        return response()->json('Image uploaded successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,124 @@
+@extends('master')
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <form
+          id="multiple-image-preview-ajax"
+          method="POST"
+          action="{{route('products.store')}}"
+          accept-charset="utf-8"
+          enctype="multipart/form-data"
+        >
+            @csrf
+            <div class="form-group">
+                <label>Ürün Adı</label>
+                <input type="text" name="name" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label>Ürün Açıklama</label>
+                <input type="text" name="description" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label>Kategori</label>
+                <select class="form-select" name="category_id">
+                    @foreach ($categories as $category)
+                    <option value={{$category->id}}>{{$category->name}}</option>
+                    @endforeach
+                  </select>
+            </div>
+            <div class="form-group">
+                <label>Marka</label>
+                <select class="form-select" name="brand_id">
+                    @foreach ($brands as $brand)
+                    <option value={{$brand->id}}>{{$brand->name}}</option>
+                    @endforeach
+                  </select>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <input
+                      type="file"
+                      name="images[]"
+                      id="images"
+                      placeholder="Choose images"
+                      multiple
+                    />
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <div class="mt-1 text-center">
+                    <div class="show-multiple-image-preview"></div>
+                  </div>
+                </div>
+              </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-success btn-sm btn-block mt-3">Ürün Ekle</button>
+            </div>
+            </form>
+        </div>
+    </div>
+@endsection
+@section('script')
+<script>
+    $(document).ready(function (e) {
+      $.ajaxSetup({
+        headers: {
+          "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
+      });
+      $(function () {
+        // Multiple images preview with JavaScript
+        var ShowMultipleImagePreview = function (
+          input,
+          imgPreviewPlaceholder
+        ) {
+          if (input.files) {
+            var filesAmount = input.files.length;
+            for (i = 0; i < filesAmount; i++) {
+              var reader = new FileReader();
+              reader.onload = function (event) {
+                $($.parseHTML("<img>"))
+                  .attr("src", event.target.result)
+                  .appendTo(imgPreviewPlaceholder);
+              };
+              reader.readAsDataURL(input.files[i]);
+            }
+          }
+        };
+        $("#images").on("change", function () {
+          ShowMultipleImagePreview(this, "div.show-multiple-image-preview");
+        });
+      });
+      $("#multiple-image-preview-ajax").submit(function (e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        let TotalImages = $("#images")[0].files.length; //Total Images
+        console.log($("#images"));
+        let images = $("#images")[0];
+        for (let i = 0; i < TotalImages; i++) {
+          formData.append("images" + i, images.files[i]);
+        }
+        formData.append("TotalImages", TotalImages);
+        $.ajax({
+          type: "POST",
+          url: "{{ url('admin/products')}}",
+          data: formData,
+          cache: false,
+          contentType: false,
+          processData: false,
+          success: (data) => {
+              console.log(data);
+            this.reset();
+            alert("Images has been uploaded using jQuery ajax with preview");
+            $(".show-multiple-image-preview").html("");
+          },
+          error: function (data) {
+            console.log(data);
+          },
+        });
+      });
+    });
+  </script>
+ 
+@endsection
